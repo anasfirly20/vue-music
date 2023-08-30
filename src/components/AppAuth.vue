c18695fb0939aceb35c8be4c888f8a6e564562c3
Anas Firly <anasfirly20@gmail.com>
2023-08-30 22:55:52 +0400

    adding conditional for login/reg tab

--- src/components/AppAuth.vue
+++ src/components/AppAuth.vue
@@ -54,7 +54,7 @@
           </ul>
 
           <!-- Login Form -->
-          <form>
+          <form v-show="tab === 'login'">
             <!-- Email -->
             <div class="mb-3">
               <label class="inline-block mb-2">Email</label>
@@ -81,7 +81,7 @@
             </button>
           </form>
           <!-- Registration Form -->
-          <form>
+          <form v-show="tab === 'register'">
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
