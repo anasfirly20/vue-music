77e5d08f1805c2a3158d5175d69dc6918804cd13
Anas Firly <anasfirly20@gmail.com>
2023-09-05 00:45:02 +0400

    adding edit data firebase

--- src/components/CompositionItem.vue
+++ src/components/CompositionItem.vue
@@ -12,6 +12,9 @@
         <i class="fa fa-pencil-alt"></i>
       </button>
     </div>
+    <div class="text-white text-center font-bold p-4 mb-4" v-if="show_alert" :class="alert_variant">
+      {{ alert_message }}
+    </div>
     <vee-form :validation-schema="schema" v-show="isShowForm" :initial-values="song" @submit="edit">
       <CustomInput
         :label="'Song Title'"
@@ -20,11 +23,18 @@
         placeholder="Enter Song Title"
       />
       <CustomInput :label="'Genre'" :name="'genre'" :type="'text'" placeholder="Enter Genre" />
-      <button type="submit" class="py-1.5 px-3 rounded text-white bg-green-600">Submit</button>
+      <button
+        type="submit"
+        class="py-1.5 px-3 rounded text-white bg-green-600"
+        :disabled="in_submission"
+      >
+        Submit
+      </button>
       <button
         type="button"
         class="py-1.5 px-3 rounded text-white bg-gray-600 ml-1"
         @click.prevent="hideForm"
+        :disabled="in_submission"
       >
         Go Back
       </button>
@@ -34,6 +44,8 @@
 
 <script>
 import CustomInput from '@/components/CustomInput.vue'
+import { db } from '@/includes/firebase'
+import { doc, updateDoc } from 'firebase/firestore'
 
 export default {
   name: 'CompositionItem',
@@ -44,7 +56,11 @@ export default {
       schema: {
         modified_name: 'required',
         genre: 'alpha_spaces'
-      }
+      },
+      in_submission: false,
+      show_alert: false,
+      alert_variant: 'bg-blue-500',
+      alert_message: 'Please wait updating song info.'
     }
   },
   props: {
@@ -60,8 +76,30 @@ export default {
     showForm() {
       this.isShowForm = true
     },
-    edit(values) {
-      console.log('Song edited', values)
+    async edit(values) {
+      console.log('values>', values.modified_name)
+
+      this.in_submission = true
+      this.show_alert = true
+      this.alert_variant = 'bg-blue-500'
+      this.alert_message = 'Please wait updating song info.'
+
+      const songRef = doc(db, 'songs', this.song.docID)
+      try {
+        await updateDoc(songRef, {
+          modified_name: values.modified_name
+        })
+      } catch (error) {
+        console.log(error)
+        this.in_submission = false
+        this.alert_variant = 'bg-red-500'
+        this.alert_message = 'Something went wrong, ty again later.'
+        return
+      }
+
+      this.in_submission = false
+      this.alert_variant = 'bg-green-500'
+      this.alert_message = 'Song info successfully updated.'
     }
   }
 }
