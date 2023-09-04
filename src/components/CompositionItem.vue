520540a70fe126b0d2148ceefc36ca33328e97df
Anas Firly <anasfirly20@gmail.com>
2023-09-04 23:55:26 +0400

    adding toggling form

--- src/components/CompositionItem.vue
+++ src/components/CompositionItem.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="border border-gray-200 p-3 mb-4 rounded">
-    <div>
-      <h4 class="inline-block text-2xl font-bold">Song Name</h4>
+    <div v-show="!isShowForm">
+      <h4 class="inline-block text-2xl font-bold">{{ song.modified_name }}</h4>
       <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right">
         <i class="fa fa-times"></i>
       </button>
-      <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right">
+      <button
+        class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right"
+        @click.prevent="showForm"
+      >
         <i class="fa fa-pencil-alt"></i>
       </button>
     </div>
     <div>
-      <form>
+      <form v-show="isShowForm">
         <div class="mb-3">
           <label class="inline-block mb-2">Song Title</label>
           <input
@@ -28,7 +31,13 @@
           />
         </div>
         <button type="submit" class="py-1.5 px-3 rounded text-white bg-green-600">Submit</button>
-        <button type="button" class="py-1.5 px-3 rounded text-white bg-gray-600">Go Back</button>
+        <button
+          type="button"
+          class="py-1.5 px-3 rounded text-white bg-gray-600"
+          @click.prevent="hideForm"
+        >
+          Go Back
+        </button>
       </form>
     </div>
   </div>
@@ -37,10 +46,23 @@
 <script>
 export default {
   name: 'CompositionItem',
+  data() {
+    return {
+      isShowForm: false
+    }
+  },
   props: {
     song: {
       type: Object,
       required: true
+    }
+  },
+  methods: {
+    hideForm() {
+      this.isShowForm = false
+    },
+    showForm() {
+      this.isShowForm = true
     }
   }
 }
