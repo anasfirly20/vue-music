252f33e904b8a9eb0d20a051dc0ab8a035367c00
Anas Firly <anasfirly20@gmail.com>
2023-09-05 10:12:15 +0400

    adding SongItem comp

---
+++ src/components/SongItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <li
+    class="flex justify-between items-center p-3 pl-6 cursor-pointer transition duration-300 hover:bg-gray-50"
+  >
+    <div class="flex flex-col">
+      <a href="#" class="font-bold block text-gray-600">{{ song.modified_name }}</a>
+      <span class="text-gray-500 text-sm">Artist Name</span>
+      <small class="text-sm" v-if="song.genre"
+        >Genre: <span class="text-slate-500">{{ song.genre }}</span></small
+      >
+    </div>
+
+    <div class="text-gray-600 text-lg">
+      <span class="comments">
+        <i class="fa fa-comments text-gray-600"></i>
+        15
+      </span>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'SongItem',
+  props: {
+    song: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
