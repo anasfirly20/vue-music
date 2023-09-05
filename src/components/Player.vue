ac8d26906909d010867ddcae974943e0bf7bc8a1
Anas Firly <anasfirly20@gmail.com>
2023-09-06 01:56:21 +0400

    adding bullet audio position

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -17,7 +17,10 @@
       <!-- Current Position -->
       <div class="player-currenttime">{{ seek }}</div>
       <!-- Scrub Container  -->
-      <div class="w-full h-2 rounded bg-gray-200 relative cursor-pointer">
+      <div
+        class="w-full h-2 rounded bg-gray-200 relative cursor-pointer"
+        @click.prevent="updateSeek"
+      >
         <!-- Player Ball -->
         <span
           class="absolute -top-2.5 -ml-2.5 text-gray-800 text-lg"
@@ -48,7 +51,14 @@ export default {
     ...mapActions(usePlayerStore, ['toggleAudio'])
   },
   computed: {
-    ...mapState(usePlayerStore, ['playing', 'duration', 'seek', 'playerProgress', 'current_song'])
+    ...mapState(usePlayerStore, [
+      'playing',
+      'duration',
+      'seek',
+      'playerProgress',
+      'current_song',
+      'updateSeek'
+    ])
   }
 }
 </script>
